abde213ef36dc8f179562dd4f0cc4ccc80b3044b
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-08-01 19:16:18 +0300

    implement user detail page

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+
+  user: any;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ){}
+
+  ngOnInit() {
+    const userId = this.route.snapshot.params['id'];
+    this.userService.getUserById(userId).subscribe(data => {
+      this.user = data.data;
+    });
+  }
+
+    goBack() {
+    this.router.navigate(['/']);
+  }
 
 }
