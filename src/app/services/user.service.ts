a08c2068bee1db6401b37ff180b39dbb7cb1239f
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-08-02 01:20:26 +0300

    Introduce caching mechanisms to avoid redundant HTTP requests

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,24 +1,49 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, catchError, delay, Observable, of, shareReplay, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly API_URL = 'https://reqres.in/api/users';
+
+  private usersCache = new Map<number, Observable<any>>();
+  private userCache = new Map<number, Observable<any>>();
+
   private searchQuerySubject = new BehaviorSubject<string>('');
   searchQuery$ = this.searchQuerySubject.asObservable();
 
 
   constructor(private http: HttpClient) { }
 
+
   getUsers(page: number): Observable<any> {
-    return this.http.get<any>(`${this.API_URL}?page=${page}`);
+    if (!this.usersCache.has(page)) {
+      const request$ = this.http.get<any>(`${this.API_URL}?page=${page}`).pipe(
+        shareReplay(1), 
+        catchError(error => {
+          console.error('Error fetching users:', error);
+          return of({ data: [], total: 0 }); 
+        })
+      );
+      this.usersCache.set(page, request$);
+    }
+    return this.usersCache.get(page) || of({ data: [], total: 0 });
   }
 
   getUserById(id: number): Observable<any> {
-    return this.http.get<any>(`${this.API_URL}/${id}`);
+    if (!this.userCache.has(id)) {
+      const request$ = this.http.get<any>(`${this.API_URL}/${id}`).pipe(
+        shareReplay(1),
+        catchError(error => {
+          console.error('Error fetching user:', error);
+          return of({ data: null });
+        })
+      );
+      this.userCache.set(id, request$);
+    }
+    return this.userCache.get(id) || of({ data: null });
   }
 
   setSearchQuery(query: string) {
